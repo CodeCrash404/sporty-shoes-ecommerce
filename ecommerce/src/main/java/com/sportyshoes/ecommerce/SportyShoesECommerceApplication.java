f0ade7c0bee1398044cec473cedc35c0fc20031a
CodeCrash404 <127882996+CodeCrash404@users.noreply.github.com>
2023-08-20 21:53:51 +0200

    Initial Spring Boot project setup

---
+++ ecommerce/src/main/java/com/sportyshoes/ecommerce/SportyShoesECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.sportyshoes.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportyShoesECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportyShoesECommerceApplication.class, args);
+	}
+
+}
